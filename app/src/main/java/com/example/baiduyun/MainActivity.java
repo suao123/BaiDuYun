503aed0e8b12c3e8478159535083fb8c7dd04317
guzeming <1285052975@qq.com>
2019-12-10 12:19:36 +0800

    开始做主界面了

--- app/src/main/java/com/example/baiduyun/MainActivity.java
+++ app/src/main/java/com/example/baiduyun/MainActivity.java
@@ -49,7 +49,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
         btn_singUp.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
